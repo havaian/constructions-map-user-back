a3dd758f8639c6d5872b4377109d9b5051961f7f
Malik <snapfirecookie@gmail.com>
2022-08-12 17:09:18 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("./app/models/index.js");
+
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+const controllers = require('./app/controllers/building.controller');
+
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/get-all-buildings", (req, res) => {
+  controllers.findAllBuildings(req, res);
+});
+ 
+app.get("/get-building/:id", (req, res) => {
+  controllers.findOneBuilding(req, res);
+});
+
+app.post('/add-building', (req, res) => {
+  controllers.addOneBuilding(req, res);
+});
+
+app.post("/update-building/:id", (req, res) => {
+  controllers.updateOneBuilding(req, res);
+});
+
+app.post("/delete-building/:id", (req, res) => {
+  controllers.deleteOneBuilding(req, res);
+});
+
+app.post("/delete-all-buildings", (req, res) => {
+  controllers.deleteAllBuildings(req, res);
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Port: ${PORT} ✅`);
+});
+
+db.mongoose.connect(db.url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("MongoDB ✅");
+})
+.catch(err => {
+  console.log("MongoDB ❌", err);
+  process.exit();
+});
